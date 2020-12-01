f64eb3f215600ef01293380b9e259bfc262c148a
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-01 14:02:19 +0200

    Added some changes to the MessageBusImplTest

--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -10,35 +10,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MessageBusImplTest {
     private MessageBusImpl testMessageBusIml;
+
     @BeforeEach
-    void setUp() {
+    void testSetUp() {
         testMessageBusIml = new MessageBusImpl();
     }
 
     //Tested it in SendEvent
     @Test
-    void subscribeEvent() {
+    void testSubscribeEvent() {
     }
+
     //Tested it in SendEvent
     @Test
-    void subscribeBroadcast() {
+    void testSubscribeBroadcast() {
     }
 
     @Test
-    void complete() {
+    void testComplete() {
         AttackEvent attackEvent = new AttackEvent();
         DummyMicroservice dummyMicroservice = new DummyMicroservice();
         Future<Boolean> dummyFuture = dummyMicroservice.sendEvent(attackEvent);
-        if(dummyFuture==null){
-            fail("Microservice is empty");
+        if (dummyFuture == null) {
+            fail("sendEvent has returned null");
         }
-        testMessageBusIml.complete(attackEvent,true);
+        testMessageBusIml.complete(attackEvent, true);
         assertTrue(dummyFuture.isDone());
         assertEquals(true, dummyFuture.get());
     }
+
     //when I use sendEvent/Broadcast the subscribed microservice will definitely have a message in his queue to fetch?
     @Test
-    void sendBroadcast() {
+    void testSendBroadcast() {
         TerminateBroadcast terminateBroadcast = new TerminateBroadcast();
         DummyMicroservice microService1 = new DummyMicroservice();
         DummyMicroservice microService2 = new DummyMicroservice();
@@ -47,6 +50,7 @@ class MessageBusImplTest {
             new Callback<TerminateBroadcast>() {
                 @Override
                 public void call(TerminateBroadcast c) {
+                    //empty callback
                 }
             };
         });
@@ -54,15 +58,15 @@ class MessageBusImplTest {
         try {
             Message message = testMessageBusIml.awaitMessage(microService2);
             assertEquals(message, terminateBroadcast);
-        }
-        catch (InterruptedException e){
-            System.out.println("InterruptedException");
+        } catch (InterruptedException exception) {
+            fail("The method awaitMessage should throw the IllegalStateException in the case\n" +
+                    "* where microservice was never registered.");
         }
     }
 
     //when I use sendEvent/Broadcast the subscribed microservice will definitely have a message in his queue to fetch?
     @Test
-    void sendEvent() {
+    void testSendEvent() {
         AttackEvent attackEvent = new AttackEvent();
         DummyMicroservice microService1 = new DummyMicroservice();
         DummyMicroservice microService2 = new DummyMicroservice();
@@ -70,50 +74,72 @@ class MessageBusImplTest {
         microService2.subscribeEvent(AttackEvent.class, (AttackEvent call) -> {
             new Callback<AttackEvent>() {
                 @Override
-                public void call(AttackEvent c) {
+                public void call(AttackEvent attackEvent1) {
+                    //empty callback
                 }
             };
         });
         microService1.sendEvent(attackEvent);
         try {
             Message message = testMessageBusIml.awaitMessage(microService2);
+            assertTrue(message instanceof AttackEvent);
             assertEquals(message, attackEvent);
-        }
-        catch (InterruptedException e){
-            System.out.println("InterruptedException");
+        } catch (InterruptedException exception) {
+            fail("The method awaitMessage should throw the IllegalStateException in the case\n" +
+                    "* where microservice was never registered.");
         }
     }
 
     @Test
-    void register() {
+    void testRegister() {
         DummyMicroservice microservice = new DummyMicroservice();
         try {
             testMessageBusIml.awaitMessage(microservice);
-        }catch (IllegalStateException | InterruptedException e){
-            fail("Test Failed");
+        } catch (InterruptedException exception) {
+            fail("The method awaitMessage should throw the IllegalStateException in the case\n" +
+                    "* where microservice was never registered.");
         }
     }
+
     //test with the assumption that the que is NOT empty (getters are not allowed)
     //test only the case where there's a message waiting to be fetched, and make sure it is indeed fetched.
     @Test
-    void awaitMessage() {
+    void testAwaitMessage() {
         DummyMicroservice microService1 = new DummyMicroservice();
         DummyMicroservice microService2 = new DummyMicroservice();
+        AttackEvent attackEvent = new AttackEvent();
+        try {
+            testMessageBusIml.awaitMessage(microService1);
+            fail("The method awaitMessage should throw the IllegalStateException in the case\n" +
+                    "* where microservice was never registered.");
+        } catch (Exception exception) {
+            assertTrue(exception instanceof IllegalStateException);
+        }
+        ;
         testMessageBusIml.register(microService1);
         testMessageBusIml.register(microService2);
         microService2.subscribeBroadcast(Broadcast.class, (Broadcast call) -> {
             new Callback<Broadcast>() {
                 @Override
-                public void call(Broadcast c) {
+                public void call(Broadcast broadcast) {
+                    //empty callback
                 }
             };
         });
-        microService1.sendBroadcast(new Broadcast() {});
-        try{
+        microService1.sendBroadcast(new Broadcast() {
+        });
+        try {
             Message message = testMessageBusIml.awaitMessage(microService2);
-            assertEquals(message, new Broadcast() {});
-        }catch (IllegalStateException | InterruptedException e){
-            fail("Test Failed");
+            if (message == null) {
+                fail("Message is null");
+            } else {
+                assertTrue(message instanceof Broadcast);
+                assertEquals(message, new Broadcast() {
+                });
+            }
+        } catch (InterruptedException exception) {
+            fail("The method awaitMessage should throw the IllegalStateException in the case\n" +
+                    "* where microservice was never registered.");
         }
     }
-    }
+}
