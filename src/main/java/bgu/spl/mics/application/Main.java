bab077a9657aa10dbf322a6aaadea23702e58d5c
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-07 13:49:09 +0200

    Added input/output reader/writer

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,14 +1,13 @@
 package bgu.spl.mics.application;
 
-import bgu.spl.mics.application.passiveObjects.Attack;
+import bgu.spl.mics.application.passiveObjects.Diary;
+import bgu.spl.mics.application.passiveObjects.Input;
+import bgu.spl.mics.application.services.LandoMicroservice;
 import bgu.spl.mics.application.services.LeiaMicroservice;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Reader;
 
 /**
@@ -17,11 +16,32 @@ import java.io.Reader;
  * In the end, you should output a JSON.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String inputFilePath = args[0]; //input file path from arguments
+        String outputFilePath = args[1];//output file path from arguments
+        Input input = null;
+        input= getInputFromJson(inputFilePath);
+        if(input!=null)
+            run(input);
+        Diary recordDiary = Diary.getInstance();
+        diaryToJson(outputFilePath,recordDiary);
+    }
+    private static void run(Input input){
+        LeiaMicroservice Leia = new LeiaMicroservice(input.getAttacks());
+        new LandoMicroservice(input.getLando());
+    }
+    private static Input getInputFromJson(String filePath) throws IOException {
         Gson gson = new Gson();
-        Reader reader = new FileReader(args[1]);
-        Input input = gson.fromJson(reader, Input.class);
-        //LeiaMicroservice Leia = new LeiaMicroservice(input.getAttacks());
+        try (Reader reader = new FileReader(filePath)) {
+            return gson.fromJson(reader, Input.class);
+        }
+    }
+    private static void diaryToJson(String filePath, Diary recordDiary) throws IOException {
+        Gson gson = new Gson();
+        try (Writer writer = new FileWriter(filePath)) {
+            gson.toJson(recordDiary, writer);
+        }
     }
 }
+
 
