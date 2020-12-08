c306e38d380350d9262c89c9b66f6617ae88552d
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 22:08:07 +0200

    Added a comment from forum

--- src/main/java/bgu/spl/mics/application/messages/TerminateBroadcast.java
+++ src/main/java/bgu/spl/mics/application/messages/TerminateBroadcast.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Broadcast;
-
+//https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=7c4c729c97b0d1b0994d76e552587914
+//terminate together
 public class TerminateBroadcast implements Broadcast {
 }
