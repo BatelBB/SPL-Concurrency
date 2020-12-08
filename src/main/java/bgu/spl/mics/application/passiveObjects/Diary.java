35258629d862d0c3c2c5b8213c85f7a4f1fcba83
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 18:05:53 +0200

    Added all of the parameter + setters + getter

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -1,6 +1,8 @@
 package bgu.spl.mics.application.passiveObjects;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Passive data-object representing a Diary - in which the flow of the battle is recorded.
  * We are going to compare your recordings with the expected recordings, and make sure that your output makes sense.
@@ -8,8 +10,62 @@ package bgu.spl.mics.application.passiveObjects;
  * Do not add to this class nothing but a single constructor, getters and setters.
  */
 public class Diary {
+    private static long beginning;
+    private static final Diary diary = new Diary();
+
+    int totalAttacks;
+    long HanSoloFinish;
+    long C3POFinish;
+    long R2D2Deactivate;
+    long LeiaTerminate;
+    long HanSoloTerminate;
+    long C3POTerminate;
+    long R2D2Terminate;
+    long LandoTerminate;
+
+    public Diary(){
+        diary.setBeginning();
+    }
 
     public static Diary getInstance() {
-        return null;
+        return diary;
     }
+
+    public void setBeginning(){
+        beginning = System.currentTimeMillis();
+    }
+
+    public void setTotalAttacks(int totalAttacks){
+        this.totalAttacks = totalAttacks;
+    }
+
+    public void setHanSoloFinishTime(){
+        HanSoloFinish = beginning - System.currentTimeMillis();
+    }
+
+    public void setC3POFinish(){
+        C3POFinish = beginning - System.currentTimeMillis();
+    }
+
+    public void setR2D2Deactivate(){
+        R2D2Deactivate = beginning - System.currentTimeMillis();
+    }
+
+    public  void setLeiaTerminate(){
+        LeiaTerminate = beginning - System.currentTimeMillis();
+    }
+
+    public  void setLandoTerminate(){
+        LandoTerminate = beginning - System.currentTimeMillis();
+    }
+    public  void setHanSoloFinish(){
+        HanSoloTerminate = beginning - System.currentTimeMillis();
+    }
+    public  void setC3POTerminate(){
+        C3POTerminate = beginning - System.currentTimeMillis();
+    }
+    public  void setR2D2Terminate(){
+        R2D2Terminate = beginning - System.currentTimeMillis();
+    }
+
 }
