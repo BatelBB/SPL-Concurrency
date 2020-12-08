0e33d97175d6b457173b404122ac445e8187ef41
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 18:08:21 +0200

    Changed the constructor and the initialize method

--- src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
@@ -6,6 +6,7 @@ import java.util.List;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.messages.AttackEvent;
+import bgu.spl.mics.application.passiveObjects.Diary;
 
 /**
  * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvent}.
@@ -18,16 +19,25 @@ import bgu.spl.mics.application.messages.AttackEvent;
 //there's only one microservice from each
 //https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=2e5ba1f89f40b2fd1c44f85cc7c04527
 public class LeiaMicroservice extends MicroService {
-	private Attack[] attacks;
+	private final Attack[] attacks;
+
 	
     public LeiaMicroservice(Attack[] attacks) {
         super("Leia");
 		this.attacks = attacks;
-        System.out.println("Leia");
+        System.out.println("Leia is here!");
+		initialize();
     }
 
     @Override
     protected void initialize() {
-    	
+        new Diary();
+
+    	for(int i=0; i < attacks.length; i++){
+            AttackEvent attack = new AttackEvent(attacks[i]);
+    	     this.sendEvent(attack);
+        }
+        System.out.println("Leia: I sent the attacks");
     }
+
 }
