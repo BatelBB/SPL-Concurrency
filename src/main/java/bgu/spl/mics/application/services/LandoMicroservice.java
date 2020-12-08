4708c9445939b349f84bb8942b38f2c2a30e3a6e
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 18:08:00 +0200

    Changed the constructor

--- src/main/java/bgu/spl/mics/application/services/LandoMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LandoMicroservice.java
@@ -10,14 +10,25 @@ import bgu.spl.mics.MicroService;
 //there's only one microservice from each
 //https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=2e5ba1f89f40b2fd1c44f85cc7c04527
 public class LandoMicroservice  extends MicroService {
+    private final long duration;
 
     public LandoMicroservice(long duration) {
         super("Lando");
-        System.out.println("Lando");
+        this.duration = duration;
+        System.out.println("Lando is here");
     }
 
     @Override
     protected void initialize() {
-       
+       LandoAttacks();
+
+    }
+
+    private void LandoAttacks(){
+        try{
+            this.wait(duration);
+        }catch (InterruptedException e){
+            System.out.println("Exception was thrown: " + e);
+        }
     }
 }
