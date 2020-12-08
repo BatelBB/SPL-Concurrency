453a44c51016e74ebbd7523021049eb7995d26b6
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 18:04:45 +0200

    Added constructors and sout

--- src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
@@ -4,5 +4,13 @@ import bgu.spl.mics.application.passiveObjects.Attack;
 //we are allowed to change the Boolean to Attack
 //https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=4c7f8c10563b38d9365ee44b3952040b
 public class AttackEvent implements Event<Boolean> {
-	
+	public Attack attack;
+
+	public AttackEvent(){
+
+    }
+	public AttackEvent(Attack attack){
+	    this.attack = attack;
+        System.out.println("The attack was created");
+    }
 }
