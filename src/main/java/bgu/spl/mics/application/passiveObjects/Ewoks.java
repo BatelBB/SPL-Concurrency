67b95db4192c89071477045ef75f9141d8504265
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-08 18:07:19 +0200

    Added synchronization

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -1,7 +1,7 @@
 package bgu.spl.mics.application.passiveObjects;
 
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Passive object representing the resource manager.
@@ -15,23 +15,26 @@ import java.util.List;
 //https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=1823489ac57e9f027ea846b1b636a554
 public class Ewoks {
     private final int numEwoks;
-    private List<Ewok> ewokList;
-    private static Ewoks instance;
+    private List<Ewok> synEwokList;
+    private static Ewoks ewokInstance;
 
     private Ewoks (int numEwoks){
         this.numEwoks = numEwoks;
         System.out.println("EWOKSSSS");
         setNumEwoks();
     }
+    public synchronized void getEwok(int[] serialNumbers){
+return;
+    }
     public static Ewoks getInstance(int numEwoks){
-        if(instance == null){
+        if(ewokInstance == null){
             synchronized (Ewoks.class){
-                if(instance == null){
-                    instance = new Ewoks(numEwoks);
+                if(ewokInstance == null){
+                    ewokInstance = new Ewoks(numEwoks);
                 }
             }
         }
-        return instance;
+        return ewokInstance;
     }
     private boolean resourceManager(int[] serialNumbers){
         for(int i = 0; i < serialNumbers.length; i++){
@@ -44,26 +47,28 @@ public class Ewoks {
         return true;
     }
 
-    public void releaseResources(int[] serialNumbers){
+    private void releaseResources(int[] serialNumbers){
         for (int serialNumber : serialNumbers) {
-            ewokList.get(serialNumbers[serialNumber]).release();
+            synEwokList.get(serialNumbers[serialNumber]).release();
         }
         //notifyAll()
     }
 
     private void setNumEwoks(){
+        List<Ewok> ewokList = null;
         for(int i = 1; i<=numEwoks; i++){
             ewokList.add(new Ewok(i));
         }
+        synEwokList = Collections.synchronizedList(ewokList);
     }
 
     private boolean isEwokAvailable(int serialNumber){
-        return ewokList.get(serialNumber).available;
+        return synEwokList.get(serialNumber).available;
     }
 
     private void assignResources(int[] serialNumbers){
         for (int serialNumber : serialNumbers) {
-            ewokList.get(serialNumbers[serialNumber]).acquire();
+            synEwokList.get(serialNumbers[serialNumber]).acquire();
         }
     }
 
