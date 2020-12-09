984b35b8cfd122ef9218f441299e6baeccfff719
Batel Shkolnik <batelshk@post.bgu.ac.il>
2020-12-09 22:23:02 +0200

    constructor and complete method and sendEvent method

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -9,9 +9,10 @@ import java.util.Queue;
  */
 public class MessageBusImpl implements MessageBus {
 
+
 	//Default constructor
 	private void MessageBusImpl(){
-
+		MessageBusImpl messageBus = new MessageBusImpl();
 	}
 
 	//https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=babcd640e5192717d1fa0d878f07a254
@@ -19,48 +20,108 @@ public class MessageBusImpl implements MessageBus {
 	public void getInstance(){
 
 	}
-	
+	/**
+	 * Subscribes {@code m} to receive {@link Event}s of type {@code type}.
+	 * <p>
+	 * @param <T>  The type of the result expected by the completed event.
+	 * @param type The type to subscribe to,
+	 * @param m    The subscribing micro-service.
+	 */
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-
+		//m.subscribeEvent(type.getClasses(), );
 	}
-
+	/**
+	 * Subscribes {@code m} to receive {@link Broadcast}s of type {@code type}.
+	 * <p>
+	 * @param type 	The type to subscribe to.
+	 * @param m    	The subscribing micro-service.
+	 */
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
 		
     }
-
+	/**
+	 * Notifies the MessageBus that the event {@code e} is completed and its
+	 * result was {@code result}.
+	 * When this method is called, the message-bus will resolve the {@link Future}
+	 * object associated with {@link Event} {@code e}.
+	 * <p>
+	 * @param <T>    The type of the result expected by the completed event.
+	 * @param e      The completed event.
+	 * @param result The resolved result of the completed event.
+	 */
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
-		
-	}
 
+		sendEvent(e).resolve(result);
+	}
+	/**
+	 * Adds the {@link Broadcast} {@code b} to the message queues of all the
+	 * micro-services subscribed to {@code b.getClass()}.
+	 * <p>
+	 * @param b 	The message to added to the queues.
+	 */
 	@Override
 	public void sendBroadcast(Broadcast b) {
 		
 	}
 
-	
+	/**
+	 * Adds the {@link Event} {@code e} to the message queue of one of the
+	 * micro-services subscribed to {@code e.getClass()} in a round-robin
+	 * fashion. This method should be non-blocking.
+	 * <p>
+	 * @param <T>    	The type of the result expected by the event and its corresponding future object.
+	 * @param e     	The event to add to the queue.
+	 * @return {@link Future<T>} object to be resolved once the processing is complete,
+	 * 	       null in case no micro-service has subscribed to {@code e.getClass()}.
+	 */
 	@Override
 	//an event sent without an already-waiting microservice indicates a wrong implementation.
 	//https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=9fc3d8cc9abf9ce6694b27318a17d457
 	public <T> Future<T> sendEvent(Event<T> e) {
-		
-        return null;
-	}
 
+		return new Future<T>();
+	}
+	/**
+	 * Allocates a message-queue for the {@link MicroService} {@code m}.
+	 * <p>
+	 * @param m the micro-service to create a queue for.
+	 */
 	//no need to check if already registered
 	//https://www.cs.bgu.ac.il/~spl211/Assignments/Assignment_2Forum?action=show-thread&id=75e8a7aea910dad3b1eaed9ce4e23a1d
 	@Override
 	public void register(MicroService m) {
 
 	}
-
+	/**
+	 * Removes the message queue allocated to {@code m} via the call to
+	 * {@link #register(bgu.spl.mics.MicroService)} and cleans all references
+	 * related to {@code m} in this message-bus. If {@code m} was not
+	 * registered, nothing should happen.
+	 * <p>
+	 * @param m the micro-service to unregister.
+	 */
 	@Override
 	public void unregister(MicroService m) {
 		
 	}
-
+	/**
+	 * Using this method, a <b>registered</b> micro-service can take message
+	 * from its allocated queue.
+	 * This method is blocking meaning that if no messages
+	 * are available in the micro-service queue it
+	 * should wait until a message becomes available.
+	 * The method should throw the {@link IllegalStateException} in the case
+	 * where {@code m} was never registered.
+	 * <p>
+	 * @param m The micro-service requesting to take a message from its message
+	 *          queue.
+	 * @return The next message in the {@code m}'s queue (blocking).
+	 * @throws InterruptedException if interrupted while waiting for a message
+	 *                              to became available.
+	 */
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
 		return null;
