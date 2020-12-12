6e0f4379cd513aa0b0c2fdf0fbf5b67a4fe4f410
Davehai <73841682+Davehai@users.noreply.github.com>
2020-12-12 19:11:16 +0200

    fix the sort

--- src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -10,6 +10,8 @@ import bgu.spl.mics.application.messages.TerminateBroadcast;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -41,8 +43,10 @@ public class HanSoloMicroservice extends MicroService {
         //subscribe to events of type attackevent
         this.subscribeEvent(AttackEvent.class, (AttackEvent Attack) -> {
 
-            //sorted list of ewoks needed for the attack
-            List<Integer> ewoks = SortEwoks(Attack.attack.getSerials());
+            //list of serial for the ewoks
+            List<Integer> ewoks = Attack.attack.getSerials();
+            //sort the list
+            Collections.sort(ewoks);
             long duration = Attack.GetDuration();
 
             //ask for the ewoks
@@ -90,71 +94,4 @@ public class HanSoloMicroservice extends MicroService {
 
     }
 
-    //MergeSort implement
-    private List<Integer> SortEwoks(List<Integer> ewoks) {
-        System.out.println("unsorted attacking ewoks" + ewoks);
-        MergeSort(ewoks, 0, ewoks.size() - 1);
-        System.out.println("sorted attacking ewoks" + ewoks);
-        return ewoks;
-    }
-
-    private void MergeSort(List<Integer> ewoks, int Start, int End) {
-        if (End > Start) {
-            int mid = (End + Start) / 2;
-            //sort the left side
-            MergeSort(ewoks, Start, mid);
-            //sort the right side
-            MergeSort(ewoks, mid+1, End);
-            //merge the sorted parts
-            merge(ewoks, Start, mid, End);
-        }
-    }
-
-    private void merge(List<Integer> ewoks, int Start, int mid, int End) {
-        // Find sizes of two Lists to be merged
-        int LeftSize = mid - Start + 1;
-        int RightSize = End - mid;
-
-        /* Create temp arrays */
-        int Left[] = new int[LeftSize];
-        int Right[] = new int[RightSize];
-
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < LeftSize; ++i)
-            Left[i] = ewoks.get(Start + i);
-        for (int j = 0; j < RightSize; ++j)
-            Right[j] = ewoks.get(mid + 1 + j);
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarry to list
-        int k = Start;
-        while (i < LeftSize && j < RightSize) {
-            if (Left[i] <= Right[j]) {
-                ewoks.set(k, Left[i]);
-                i++;
-            } else {
-                ewoks.set(k, Right[j]);
-                j++;
-            }
-            k++;
-        }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < LeftSize) {
-            ewoks.set(k, Left[i]);
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of R[] if any */
-        while (j < RightSize) {
-            ewoks.set(k, Right[j]);
-            j++;
-            k++;
-        }
-    }
 }
